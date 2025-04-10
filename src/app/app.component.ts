dd813c96e6bf47d0404ac472af3af9ce01e77ed2
jhoel2002 <n00254208@upn.pe>
2025-04-10 05:50:58 +0000

    Integracion de Variables de entorno

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { environment } from '../environments/environment';
+
 
 @Component({
   selector: 'app-root',
@@ -10,4 +12,8 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'claimhere-front';
+
+  ngOnInit(): void {
+    console.log('Base URL:', environment.baseUrl);  // Debería mostrar la URL correcta según el entorno
+  }
 }
